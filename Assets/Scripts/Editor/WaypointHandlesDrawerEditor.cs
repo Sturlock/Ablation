fb9da3d390ff345272224cde004e46c3e95a3a40
Sturlock <willsturley@yahoo.co.uk>
2021-10-28 10:27:05 +0100

    Draws Waypoint Handles

--- Assets/Scripts/Editor/WaypointHandlesDrawerEditor.cs
+++ Assets/Scripts/Editor/WaypointHandlesDrawerEditor.cs
@@ -3,14 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
-[UnityEditor.CustomEditor(typeof(MoveToTarget))]
+[UnityEditor.CustomEditor(typeof(MoveToTarget))] [CanEditMultipleObjects]
 public class WaypointHandlesDrawerEditor : Editor
 {
     protected MoveToTarget character;
     protected bool drawWaypointsHandles = true;
-    
+
+    SerializedProperty characterProperty;
+
+    void OnEnable()
+    {
+        //     characterProperty = serializedObject.FindProperty("lookAtPoint");
+        character = serializedObject.targetObject as MoveToTarget;
+    }
+
     protected virtual void OnSceneGUI()
     {
+        
         if (!drawWaypointsHandles) return;
         Debug.Log("DRAW");
         
