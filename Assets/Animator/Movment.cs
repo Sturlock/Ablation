9cda50e9b479b9f6d72ee93fd46c786f9dee1624
Ellen <ellentaylor26.et@gmail.com>
2022-01-25 13:24:08 +0000

    Added animation Trees: Blend and Standard
    Animations not working within project, works isolated.

---
+++ Assets/Animator/Movment.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movment : MonoBehaviour
+{
+    Animator animator;
+    public bool CrouchToggle = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        Debug.Log(animator);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool IsWalking = animator.GetBool("IsWalking");
+        bool IsRunning = animator.GetBool("IsRunning");
+        bool IsCrouching = animator.GetBool("IsCrouching");
+        bool walking = Input.GetKey("w");
+        bool running = Input.GetKey(KeyCode.LeftShift);
+        bool Crouch = Input.GetKeyDown(KeyCode.LeftControl);
+
+        if (!IsCrouching && Crouch)
+        {
+            Crouching();
+        }
+
+        if (CrouchToggle == true)
+        {
+            if (!IsWalking && walking)
+            {
+                animator.SetBool("IsWalking", true);
+            }
+            if (IsWalking && !walking)
+            {
+                animator.SetBool("IsWalking", false);
+            }
+
+            if (IsWalking && !IsRunning && running)
+            {
+                animator.SetBool("IsRunning", true);
+                Crouching();
+            }
+        }
+
+        if (IsCrouching && Crouch)
+        {
+            Crouching();
+        }
+
+        if (!IsWalking && walking)
+        {
+            animator.SetBool("IsWalking", true);
+        }
+
+        if (IsWalking && !IsRunning && running)
+        {
+            animator.SetBool("IsRunning", true); 
+        }
+
+        if (IsWalking && IsRunning && !running)
+        {
+            animator.SetBool("IsRunning", false);
+        }
+
+        if (IsWalking && !walking)
+        {
+            animator.SetBool("IsWalking", false);
+            animator.SetBool("IsRunning", false);
+        }
+    }
+
+    private void Crouching()
+    {
+        CrouchToggle = !CrouchToggle;
+        Debug.Log(CrouchToggle);
+        animator.SetBool("IsCrouching", CrouchToggle);
+        //animator.SetBool("IsRunning", false);
+
+        
+    }
+}
